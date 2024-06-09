d97a512c4c18dec506221a6840656eeb233dc621
solomon-ojo <woleojosolomon@gmail.com>
2024-06-09 17:05:05 +0200

    commit

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   // detect whether user has scrolled the page down by 10px
   const scrollHandler = () => {
     window.pageYOffset > 10 ? setTop(false) : setTop(true)
-  }  
+  }
 
   useEffect(() => {
     scrollHandler()
@@ -60,12 +60,12 @@ export default function Header() {
                <li>
                 <Link href="https://wa.me/+447342920067"  className="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">Contact</Link>
               </li>
-              
+
               {/* 1st level: hover */}
 
               {/* 2nd level: hover */}
               {/* <Dropdown title="Resources">
-                
+
                  <li>
                   <Link href="/documentation" className="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">Documentation</Link>
                 </li>
@@ -74,15 +74,15 @@ export default function Header() {
                 </li>
                 <li>
                   <Link href="/404" className="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">404</Link>
-                </li> 
+                </li>
               </Dropdown> */}
             </ul>
 
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
-              
+
               <li>
-                <Link href="/oami-ways-to-give.pdf" locale={false} target='_blank'  className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdCIKsHRnOBMPsc4wykbPCSdv91Fzr1l0HZVru5HOycVN5kUQ/viewform?usp=sf_link" locale={false} target='_blank'  className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
                   <span>Give</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
